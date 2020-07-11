8093b7406550106af7f2c41e6f985645ab906a52
Praful chandra <praful.chandra@yahoo.com>
2020-07-11 15:01:15 +0000

    lesson 9

--- books.sql
+++ books.sql
@@ -123,3 +123,16 @@
 # SELECT title,author_lname FROM books ORDER BY author_lname,title;
 
 # SELECT UPPER(CONCAT('MY FAVOURITE AUTHOR IS ',author_fname,' ',author_lname,'!')) AS 'yell' FROM books ORDER BY author_lname;
+
+
+# SELECT COUNT(*) FROM books;
+
+# SELECT released_year , COUNT(*) FROM books GROUP BY released_year;
+
+# SELECT SUM(stock_quantity) AS 'total books in stock' FROM books ;
+
+# select CONCAT(author_fname,' ',author_lname) AS 'author', AVG(released_year) AS 'average released year' FROM books GROUP BY author_fname,author_lname;
+
+# SELECT CONCAT(author_fname,' ',author_lname) AS 'full name',pages FROM books ORDER BY pages DESC LIMIT 1;
+
+# SELECT released_year AS 'year',COUNT(*) AS '# books',AVG(pages) AS 'avg pages' FROM books GROUP BY released_year;
